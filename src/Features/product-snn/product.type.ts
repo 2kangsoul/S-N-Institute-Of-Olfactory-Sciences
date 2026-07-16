a4c966e5e46c555998683eada1910741f6b4f91c
nicholasng <nicholasngnicholasng29206@gmail.com>
2026-07-16 19:32:44 +0700

    feat: product integration

---
+++ src/Features/product-snn/product.type.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: string;
+  name: string;
+  brand: string;
+  type: string;
+  description: string;
+  price: number;
+  stock: number;
+  imageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+}
